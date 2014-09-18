029d11982e7fee5505653544a039ba38b501c1b7
Peter Andreus <peter.andreus@gmail.com>
2014-09-18 15:26:02 +0200

    Part casti, uprava projektov

---
+++ scriptCreator.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd scriptParts/
+
+cat userCustomPropertiesPart configPart deployPart buildPart buildDeployPart infoPart ejbPart machineControlPart svnPart updatePart mainPart> ../wasControl.sh
